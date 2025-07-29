ea32a63d2c55ad58857337d9ecda93b11d989a0b
DESKTOP-HA97GMISSAFY <yeeunyoo99@naver.com>
2025-07-29 21:25:36 +0900

    [Feat] Initialize Spring Boot project on back branch

---
+++ src/main/java/com/example/whereshouldwego/WhereShouldWeGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.whereshouldwego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereShouldWeGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereShouldWeGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whereshouldwego/WhereShouldWeGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whereshouldwego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereShouldWeGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
