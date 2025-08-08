8c19baa219c1a2c46ab77461f77d161492bf7bb3
GomDue <hdh026163@gmail.com>
2025-08-09 00:27:33 +0900

    [Chore] Change EndPoint

--- src/main/java/com/example/whereshouldwego/config/WebSocketConfig.java
+++ src/main/java/com/example/whereshouldwego/config/WebSocketConfig.java
@@ -47,7 +47,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic", "/queue");
 
-        registry.setApplicationDestinationPrefixes("/ws/send");
+        registry.setApplicationDestinationPrefixes("/ws");
     }
 
     @Override
