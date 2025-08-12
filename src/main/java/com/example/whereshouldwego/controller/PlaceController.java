988639d3482263273530a79561f4f283b7e82763
DESKTOP-HA97GMISSAFY <yeeunyoo99@naver.com>
2025-08-12 11:15:55 +0900

    [Fix] Place Controller api 수정

--- src/main/java/com/example/whereshouldwego/controller/PlaceController.java
+++ src/main/java/com/example/whereshouldwego/controller/PlaceController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/places")
+@RequestMapping("/api/places")
 @RequiredArgsConstructor
 public class PlaceController {
     private final PlaceService placeService;
