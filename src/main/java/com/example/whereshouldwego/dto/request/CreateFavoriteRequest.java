a18ac7c975d78ef1616e45196af7b9bf4d1cc820
DESKTOP-HA97GMISSAFY <yeeunyoo99@naver.com>
2025-08-12 10:27:30 +0900

    [Feat] DB에 없는 장소를 찜하거나 후보지 등록할 때 발생하는 문제 해결. DB에 없는 place data는 db에 선 적재 후 반환하게끔 수정함

--- src/main/java/com/example/whereshouldwego/dto/request/CreateFavoriteRequest.java
+++ src/main/java/com/example/whereshouldwego/dto/request/CreateFavoriteRequest.java
@@ -4,10 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class CreateFavoriteRequest {
     private Long userId;
     private Long placeId;
+    private Long kakaoPlaceId;
+
+    private String name;
+    private Double x;
+    private Double y;
+    private String address;
+    private String roadAddress;
+    private String phone;
+    private String categoryCode;
+    private String categoryName;
+    private String kakaoUrl;
 }
--- src/main/java/com/example/whereshouldwego/service/PlaceService.java
+++ src/main/java/com/example/whereshouldwego/service/PlaceService.java
@@ -1,18 +1,30 @@
 package com.example.whereshouldwego.service;
 
 import com.example.whereshouldwego.domain.Place;
+import com.example.whereshouldwego.dto.request.CreateFavoriteRequest;
+import com.example.whereshouldwego.dto.request.EnsurePlacesRequest;
 import com.example.whereshouldwego.dto.response.PlaceResponse;
 import com.example.whereshouldwego.repository.postgres.PlaceRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PersistenceException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLOutput;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PlaceService {
     private final PlaceRepository placeRepository;
+    @PersistenceContext
+    private EntityManager em;
     // 전체 장소  조회
     public List<PlaceResponse> getAllPlaces() {
         List<Place> places = placeRepository.findAll();
@@ -27,4 +39,32 @@ public class PlaceService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 장소가 존재하지 않습니다."));
         return PlaceResponse.fromEntity(place);
     }
+
+    @Transactional
+    public List<PlaceResponse> saveIfAbsentAll(Collection<Place> candidates) {
+        if (candidates == null || candidates.isEmpty()) {
+            return List.of();
+        }
+        for (Place candidate : candidates) {
+            placeRepository.insertOrIgnore(
+                    candidate.getId(),
+                    candidate.getName(),
+                    candidate.getKakaoUrl(),
+                    candidate.getX(),
+                    candidate.getY(),
+                    candidate.getAddress(),
+                    candidate.getRoadAddress(),
+                    candidate.getPhone(),
+                    candidate.getCategoryCode(),
+                    candidate.getCategoryName()
+            );
+        }
+        Set<Long> insertedIds = candidates.stream().map(Place::getId).collect(Collectors.toSet());
+        List<Place> insertedPlaces = placeRepository.findAllById(insertedIds);
+
+        return insertedPlaces.stream()
+                .map(PlaceResponse::fromEntity)
+                .toList();
+    }
+
 }
